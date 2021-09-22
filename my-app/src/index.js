267f2efcf24b10344a06efa8a1f2f561ffefcef1
Isuru Dhanisha <Isurudhanisha@gmail.com>
2021-09-22 06:16:11 +0530

    cart,googlepay,up.adreess

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignUp from "./components/signup";
 import SignIn from "./components/signin";
+import Cart from "./components/cart";
 import NavBar from "./components/navbar";
 import Product from "./components/products";
 import addProduct from "./components/addProduct";
 import updateProduct from "./components/updateProduct";
+import updateAddress from "./components/updateAddress";
+import GooglePay from "./components/googlePay";
 axios.defaults.baseURL = "http://localhost:5000/";
 
 ReactDOM.render(
@@ -19,7 +22,9 @@ ReactDOM.render(
     <Route exact path="/signin" component={SignIn} />
     <Route exact path="/addProduct" component={addProduct} />
     <Route exact path="/updateProduct" component={updateProduct} />
-    
+    <Route exact path="/cart" component={Cart} />
+    <Route exact path="/updateAddress" component={updateAddress} />
+    <Route exact path="/googlePay" component={GooglePay} />    
     </BrowserRouter>,
   document.getElementById('root')
 );
